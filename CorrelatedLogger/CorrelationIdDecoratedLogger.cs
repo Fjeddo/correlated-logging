d8e61944f955f274ef675ad15e6fccc8dc6fb327
Fredde Johnsson <fredrik.johnsson7@gmail.com>
2022-03-16 21:34:17 +0100

    Support for structured logging, with message templates.

--- CorrelatedLogger/CorrelationIdDecoratedLogger.cs
+++ CorrelatedLogger/CorrelationIdDecoratedLogger.cs
@@ -13,9 +13,15 @@ public class CorrelationIdDecoratedLogger<T> : ILogger<T>
         _logger = factory.CreateLogger<T>();
         _correlationIdProvider = correlationIdProvider;
     }
-    
+
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception, string> formatter) =>
-        _logger.Log(logLevel, eventId, exception, "{stateFormatter} (correlationId={correlationId})", formatter(state, exception!), _correlationIdProvider.CorrelationId);
+        Log(logLevel,
+            eventId,
+            (state as IEnumerable<KeyValuePair<string, object>> ?? Array.Empty<KeyValuePair<string, object>>()).Append(new KeyValuePair<string, object>("correlationId", _correlationIdProvider.CorrelationId)),
+            exception,
+            (_, e) => $"{formatter(state, e)} (correlationId={_correlationIdProvider.CorrelationId})".TrimStart());
+
+    private void Log(LogLevel logLevel, EventId eventId, IEnumerable<KeyValuePair<string, object>> state, Exception? exception, Func<IEnumerable<KeyValuePair<string, object>>, Exception, string> formatter) => _logger.Log(logLevel, eventId, state, exception, formatter!);
 
     public bool IsEnabled(LogLevel logLevel) => _logger.IsEnabled(logLevel);
 
